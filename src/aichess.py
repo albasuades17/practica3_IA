a28d81c64a55a9318d6c2b49fcf2a9bbb0398fb8
DavMol2001 <dmolinba24@alumnes.ub.edu>
2022-10-06 16:15:38 +0200

    Initial commit

---
+++ src/aichess.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 11:22:03 2022
+
+@author: ignasi
+"""
+import copy
+
+import chess
+import numpy as np
+import sys
+import queue
+from typing import List
+
+RawStateType = List[List[List[int]]]
+
+from itertools import permutations
+
+
+class Aichess():
+    """
+    A class to represent the game of chess.
+
+    ...
+
+    Attributes:
+    -----------
+    chess : Chess
+        represents the chess game
+
+    Methods:
+    --------
+    startGame(pos:stup) -> None
+        Promotes a pawn that has reached the other side to another, or the same, piece
+
+    """
+
+    def __init__(self, TA, myinit=True):
+
+        if myinit:
+            self.chess = chess.Chess(TA, True)
+        else:
+            self.chess = chess.Chess([], False)
+
+        self.listNextStates = []
+        self.listVisitedStates = []
+        self.pathToTarget = []
+        self.currentStateW = self.chess.boardSim.currentStateW;
+        self.depthMax = 8;
+        self.checkMate = False
+
+    def getCurrentState(self):
+
+        return self.myCurrentStateW
+
+    def getListNextStatesW(self, myState):
+
+        self.chess.boardSim.getListNextStatesW(myState)
+        self.listNextStates = self.chess.boardSim.listNextStates.copy()
+
+        return self.listNextStates
+
+    def getListNextStatesB(self, myState):
+        self.chess.boardSim.getListNextStatesB(myState)
+        self.listNextStates = self.chess.boardSim.listNextStates.copy()
+
+        return self.listNextStates
+
+    def isSameState(self, a, b):
+
+        isSameState1 = True
+        # a and b are lists
+        for k in range(len(a)):
+
+            if a[k] not in b:
+                isSameState1 = False
+
+        isSameState2 = True
+        # a and b are lists
+        for k in range(len(b)):
+
+            if b[k] not in a:
+                isSameState2 = False
+
+        isSameState = isSameState1 and isSameState2
+        return isSameState
+
+    def isVisited(self, mystate):
+
+        if (len(self.listVisitedStates) > 0):
+            perm_state = list(permutations(mystate))
+
+            isVisited = False
+            for j in range(len(perm_state)):
+
+                for k in range(len(self.listVisitedStates)):
+
+                    if self.isSameState(list(perm_state[j]), self.listVisitedStates[k]):
+                        isVisited = True
+
+            return isVisited
+        else:
+            return False
+
+    def isCheckMateBlack(self, whiteState, blackState):
+        bkState = self.getPieceState(blackState, 12)
+        wkState = self.getPieceState(whiteState, 6)
+        wrState = self.getPieceState(whiteState, 2)
+        checkMate = False
+        #Rei negre es troba a una paret
+        if bkState[0] == 0 or bkState[0] == 7 or bkState[1] == 0 or bkState[1] == 7:
+            #El mètode ens retorna llista de [fila,columna]
+            nextBkStates = self.getNextPositions(bkState)
+            nextWkStates = self.getNextPositions(wkState)
+            nextWrStates = self.getNextPositions(wrState)
+            print("BK positions: ", nextBkStates)
+            print("WK positions: ", nextWkStates)
+            print("WR positions: ", nextWrStates)
+            checkMate = True
+            for state in nextBkStates:
+                if state not in nextWkStates and state not in nextWrStates:
+                    checkMate = False
+                    break
+
+        return checkMate
+
+    def getPieceState(self, state, piece):
+        pieceState = None
+        for i in state:
+            if i[2] == piece:
+                pieceState = i
+                break
+        return pieceState
+
+    def getNextPositions(self, state):
+        if state[2] > 6:
+            nextStates = self.getListNextStatesB([state])
+        else:
+            nextStates = self.getListNextStatesW([state])
+        nextPositions = []
+        for i in nextStates:
+            nextPositions.append(i[0:2])
+        return nextPositions
+
+def translate(s):
+    """
+    Translates traditional board coordinates of chess into list indices
+    """
+
+    try:
+        row = int(s[0])
+        col = s[1]
+        if row < 1 or row > 8:
+            print(s[0] + "is not in the range from 1 - 8")
+            return None
+        if col < 'a' or col > 'h':
+            print(s[1] + "is not in the range from a - h")
+            return None
+        dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+        return (8 - row, dict[col])
+    except:
+        print(s + "is not in the format '[number][letter]'")
+        return None
+
+
+if __name__ == "__main__":
+    #   if len(sys.argv) < 2:
+    #       sys.exit(usage())
+
+    # intiialize board
+    TA = np.zeros((8, 8))
+    # white pieces
+    # TA[0][0] = 2
+    # TA[2][4] = 6
+    # # black pieces
+    # TA[0][4] = 12
+
+    TA[7][7] = 2
+    TA[7][5] = 6
+    #TA[0][7] = 8
+    TA[4][7] = 12
+
+    # initialise board
+    print("stating AI chess... ")
+    aichess = Aichess(TA, True)
+    currentState = aichess.chess.board.currentStateW.copy()
+
+    print("printing board")
+    aichess.chess.boardSim.print_board()
+
+    # get list of next states for current state
+    print("current State", currentState)
+
+    #call checkmate method
+    whiteState = aichess.chess.board.currentStateW.copy()
+    blackState = aichess.chess.board.currentStateB.copy()
+    aichess.isCheckMateBlack(whiteState, blackState)
